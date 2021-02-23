636a968809252b55ee76c13970eaac80fb91f558
plummm <etenalcxz@gmail.com>
2021-02-23 15:50:21 -0800

    try to fix a wired bug in fromVultoAssigntoUse()

--- llvm_analysis/MainAnalysisPasses/SoundyAliasAnalysis/src/TaintAnalysisVisitor.cpp
+++ llvm_analysis/MainAnalysisPasses/SoundyAliasAnalysis/src/TaintAnalysisVisitor.cpp
@@ -294,19 +294,20 @@ namespace DRCHECKER {
         for(; tmp < usecallsites->size(); tmp++){
             cpusecallsites.push_back((*usecallsites)[tmp]);
         }
-        errs() << "cpvulcallsites " << cpvulcallsites.size() << "\n";
         if (cpvulcallsites.size() > 1) {
             for(auto idx = cpvulcallsites.size() - 1; idx > 0; idx -= 2){
                 PathfromVultoUse->push_back(cpvulcallsites[idx]);
                 auto getret = new getRet();
-                getret->BBvisit(cpvulcallsites[idx]->getParent(), 0);
-                if(getret->found){
-                    PathfromVultoUse->push_back(getret->ret);
-                }else{
-                    errs() << "unable to find a ret inst!\n";
-                    return;
+                errs() << "cpvulcallsites " << cpvulcallsites[idx] << "\n";
+                if (cpvulcallsites[idx]) {
+                    getret->BBvisit(cpvulcallsites[idx]->getParent(), 0);
+                    if(getret->found){
+                        PathfromVultoUse->push_back(getret->ret);
+                    }else{
+                        errs() << "unable to find a ret inst!\n";
+                        return;
+                    }
                 }
-                
             }
         }
         PathfromVultoUse->push_back(cpvulcallsites[0]);
@@ -1012,19 +1013,20 @@ namespace DRCHECKER {
         for(; tmp < assigncallsites->size(); tmp++){
             cpasscallsites_a.push_back((*assigncallsites)[tmp]);
         }
-        errs() << "cpvulcallsites " << cpvulcallsites.size() << "\n";
         if (cpvulcallsites.size() > 1) {
             for(auto idx = cpvulcallsites.size() - 1; idx > 0; idx -= 2){
                 PathfromVultoAss->push_back(cpvulcallsites[idx]);
                 auto getret = new getRet();
-                getret->BBvisit(cpvulcallsites[idx]->getParent(), 0);
-                if(getret->found){
-                    PathfromVultoAss->push_back(getret->ret);
-                }else{
-                    errs() << "unable to find a ret inst!\n";
-                    return;
-                }
-                
+                errs() << "cpvulcallsites " << cpvulcallsites[idx] << "\n";
+                if (cpvulcallsites[idx]) {
+                    getret->BBvisit(cpvulcallsites[idx]->getParent(), 0);
+                    if(getret->found){
+                        PathfromVultoAss->push_back(getret->ret);
+                    }else{
+                        errs() << "unable to find a ret inst!\n";
+                        return;
+                    }
+                }           
             }
         }
         PathfromVultoAss->push_back(cpvulcallsites[0]);
